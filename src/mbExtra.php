b047c6facf46dd9efd2dada05f2187dbb8a72f6c
Danack <Danack@basereality.com>
2013-06-02 14:34:27 +0100

    Added mb_str_replace

--- src/mbExtra.php
+++ src/mbExtra.php
@@ -92,4 +92,43 @@ function mb_wordwrap($string, $width=75, $break="\n", $cut=false)
 
 
 
+/**
+ * Replace all occurrences of the search string with the replacement string.
+ *
+ * @author Sean Murphy <sean@iamseanmurphy.com>
+ * @copyright Copyright 2012 Sean Murphy. All rights reserved.
+ * @license http://creativecommons.org/publicdomain/zero/1.0/
+ * @link http://php.net/manual/function.str-replace.php
+ *
+ * @param mixed $search
+ * @param mixed $replace
+ * @param mixed $subject
+ * @param int $count
+ * @return mixed
+ */
+function mb_str_replace($search, $replace, $subject, &$count = 0) {
+	if (!is_array($subject)) {
+		// Normalize $search and $replace so they are both arrays of the same length
+		$searches = is_array($search) ? array_values($search) : array($search);
+		$replacements = is_array($replace) ? array_values($replace) : array($replace);
+		$replacements = array_pad($replacements, count($searches), '');
+
+		foreach ($searches as $key => $search) {
+			$parts = mb_split(preg_quote($search), $subject);
+			$count += count($parts) - 1;
+			$subject = implode($replacements[$key], $parts);
+		}
+	} else {
+		// Call mb_str_replace for each subject in array, recursively
+		foreach ($subject as $key => $value) {
+			$subject[$key] = mb_str_replace($search, $replace, $value, $count);
+		}
+	}
+
+	return $subject;
+}
+
+
+
+
 ?>
