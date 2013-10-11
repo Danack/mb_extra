61655b2ceee1d74266a348375451ad3c30257f63
Danack <Danack@basereality.com>
2013-10-11 19:05:59 +0100

    Added initial travis info.

--- src/Intahwebz/MBExtra/Functions.php
+++ src/Intahwebz/MBExtra/Functions.php
@@ -4,8 +4,8 @@ namespace Intahwebz\MBExtra{
 
 
 class Functions{
-	public static function load(){
-	}
+    public static function load(){
+    }
 }
 
 }
@@ -18,96 +18,104 @@ namespace {
 // all these functions are meant to be the mb_ equivalent of their non-multi-byte-safe versions.
 // If they are not, then they have a bug. Please consider fixing and sending a pull request.
 
-function mb_ucfirst($string){
-	return mb_convert_case($string, MB_CASE_UPPER);
+function mb_ucfirst($string) {
+    $first = mb_substr($string, 0, 1);
+    $remaining = mb_substr($string, 1);
+
+    return mb_strtoupper($first).$remaining;
 }
 
-function mb_ucwords($string){
-	return mb_convert_case($string, MB_CASE_TITLE);
+function mb_ucwords($string) {
+    return mb_convert_case($string, MB_CASE_TITLE);
 }
 
 function mb_lcfirst($str) {
-	return mb_strtolower(mb_substr($str,0,1)).mb_substr($str,1);
+    return mb_strtolower(mb_substr($str,0,1)).mb_substr($str,1);
 }
 
 
 function mb_str_split($str, $l = 0) {
-	if ($l > 0) {
-		$ret = array();
-		$len = mb_strlen($str, "UTF-8");
-		for ($i = 0; $i < $len; $i += $l) {
-			$ret[] = mb_substr($str, $i, $l, "UTF-8");
-		}
-		return $ret;
-	}
-	return preg_split("//u", $str, -1, PREG_SPLIT_NO_EMPTY);
+    if ($l > 0) {
+        $ret = array();
+        $len = mb_strlen($str, "UTF-8");
+        for ($i = 0; $i < $len; $i += $l) {
+            $ret[] = mb_substr($str, $i, $l, "UTF-8");
+        }
+        return $ret;
+    }
+    return preg_split("//u", $str, -1, PREG_SPLIT_NO_EMPTY);
 }
 
 function mb_strcasecmp($str1, $str2, $encoding = null) {
-	if (null === $encoding) {
-		$encoding = mb_internal_encoding();
-	}
+    if (null === $encoding) {
+        $encoding = mb_internal_encoding();
+    }
 
-	return strcmp(mb_strtoupper($str1, $encoding), mb_strtoupper($str2, $encoding));
+    return strcmp(mb_strtoupper($str1, $encoding), mb_strtoupper($str2, $encoding));
 }
 
 function mb_strrev($str){
-	preg_match_all('/./us', $str, $ar);
-	return join('',array_reverse($ar[0]));
+    preg_match_all('/./us', $str, $ar);
+    return join('',array_reverse($ar[0]));
 }
 
 //Taken from http://www.php.net/manual/en/function.substr-replace.php#90146
 function mb_substr_replace($string, $replacement, $start, $length = null, $encoding = null)
 {
-	$string_length = (is_null($encoding) === true) ? mb_strlen($string) : mb_strlen($string, $encoding);
+    $string_length = (is_null($encoding) === true) ? mb_strlen($string) : mb_strlen($string, $encoding);
 
-	if ($start < 0)
-	{
-		$start = max(0, $string_length + $start);
-	}
-	else if ($start > $string_length)
-	{
-		$start = $string_length;
-	}
+    if ($start < 0)
+    {
+        $start = max(0, $string_length + $start);
+    }
+    else if ($start > $string_length)
+    {
+        $start = $string_length;
+    }
 
-	if ($length < 0)
-	{
-		$length = max(0, $string_length - $start + $length);
-	}
-	else if ((is_null($length) === true) || ($length > $string_length))
-	{
-		$length = $string_length;
-	}
+    if ($length < 0)
+    {
+        $length = max(0, $string_length - $start + $length);
+    }
+    else if ((is_null($length) === true) || ($length > $string_length))
+    {
+        $length = $string_length;
+    }
 
-	if (($start + $length) > $string_length)
-	{
-		$length = $string_length - $start;
-	}
+    if (($start + $length) > $string_length)
+    {
+        $length = $string_length - $start;
+    }
 
-	if (is_null($encoding) === true)
-	{
-		return mb_substr($string, 0, $start) . $replacement . mb_substr($string, $start + $length, $string_length - $start - $length);
-	}
+    if (is_null($encoding) === true)
+    {
+        return mb_substr($string, 0, $start) . $replacement . mb_substr($string, $start + $length, $string_length - $start - $length);
+    }
 
-	return mb_substr($string, 0, $start, $encoding) . $replacement . mb_substr($string, $start + $length, $string_length - $start - $length, $encoding);
+    return mb_substr($string, 0, $start, $encoding) . $replacement . mb_substr($string, $start + $length, $string_length - $start - $length, $encoding);
 }
 
 function mb_wordwrap($string, $width=75, $break="\n", $cut=false)
 {
-	$width = intval($width); //Used in match - don't trust input
+    $width = intval($width); //Used in match - don't trust input
 
-	if($cut) {
-		// Match anything 1 to $width chars long followed by whitespace or EOS,
-		// otherwise match anything $width chars long
-		$search = '/(.{1,'.$width.'})(?:\s|$)|(.{'.$width.'})/uS';
-		$replace = '$1$2'.$break;
-	} else {
-		// Anchor the beginning of the pattern with a lookahead
-		// to avoid crazy backtracking when words are longer than $width
-		$pattern = '/(?=\s)(.{1,'.$width.'})(?:\s|$)/uS';
-		$replace = '$1'.$break;
-	}
-	return preg_replace($search, $replace, $string);
+    if($cut) {
+        // Match anything 1 to $width chars long followed by whitespace or EOS,
+        // otherwise match anything $width chars long
+        $search = '/(.{1,'.$width.'})(?:\s|$)|(.{'.$width.'})/uS';
+        $replace = '$1$2'.$break;
+    } else {
+        // Anchor the beginning of the pattern with a lookahead
+        // to avoid crazy backtracking when words are longer than $width
+        $search = '/(?=\s)(.{1,'.$width.'})(?:\s|$)/uS';
+        $replace = '$1'.$break;
+    }
+    
+    $result = preg_replace($search, $replace, $string);
+    
+    $result = mb_substr($result, 0, strlen($result) - 1);
+    return $result;
+    
 }
 
 
@@ -127,25 +135,25 @@ function mb_wordwrap($string, $width=75, $break="\n", $cut=false)
  * @return mixed
  */
 function mb_str_replace($search, $replace, $subject, &$count = 0) {
-	if (!is_array($subject)) {
-		// Normalize $search and $replace so they are both arrays of the same length
-		$searches = is_array($search) ? array_values($search) : array($search);
-		$replacements = is_array($replace) ? array_values($replace) : array($replace);
-		$replacements = array_pad($replacements, count($searches), '');
+    if (!is_array($subject)) {
+        // Normalize $search and $replace so they are both arrays of the same length
+        $searches = is_array($search) ? array_values($search) : array($search);
+        $replacements = is_array($replace) ? array_values($replace) : array($replace);
+        $replacements = array_pad($replacements, count($searches), '');
 
-		foreach ($searches as $key => $search) {
-			$parts = mb_split(preg_quote($search), $subject);
-			$count += count($parts) - 1;
-			$subject = implode($replacements[$key], $parts);
-		}
-	} else {
-		// Call mb_str_replace for each subject in array, recursively
-		foreach ($subject as $key => $value) {
-			$subject[$key] = mb_str_replace($search, $replace, $value, $count);
-		}
-	}
+        foreach ($searches as $key => $search) {
+            $parts = mb_split(preg_quote($search), $subject);
+            $count += count($parts) - 1;
+            $subject = implode($replacements[$key], $parts);
+        }
+    } else {
+        // Call mb_str_replace for each subject in array, recursively
+        foreach ($subject as $key => $value) {
+            $subject[$key] = mb_str_replace($search, $replace, $value, $count);
+        }
+    }
 
-	return $subject;
+    return $subject;
 }
 
 
---
+++ src/Intahwebz/MBExtra/Tests/FunctionsTest.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace Intahwebz\Tests\MBExtra;
+
+
+
+
+class MBExtraTest extends \PHPUnit_Framework_TestCase {
+
+
+    protected function setUp(){
+        \Intahwebz\MBExtra\Functions::load();
+    }
+
+    protected function tearDown(){
+        //ob_end_clean();
+    }   
+
+
+    function testmb_ucfirst() {
+        $this->assertEquals('Daniel', mb_ucfirst('daniel'));
+        $this->assertEquals('Daniel', mb_ucfirst('Daniel'));
+
+        $this->assertEquals('Daniel ackroyd', mb_ucfirst('daniel ackroyd'));
+    }
+
+    function testmb_lcfirst() {
+        $this->assertEquals('daniel', mb_lcfirst('daniel'));
+        $this->assertEquals('daniel', mb_lcfirst('Daniel'));
+    }
+
+//$str = "Τάχιστη αλώπηξ βαφής ψημένη γη, δρασκελίζει υπέρ νωθρού κυνός";
+//$str = mb_convert_case($str, MB_CASE_UPPER, "UTF-8");
+//echo $str; // Prints ΤΆΧΙΣΤΗ ΑΛΏΠΗΞ ΒΑΦΉΣ ΨΗΜΈΝΗ ΓΗ, ΔΡΑΣΚΕΛΊΖΕΙ ΥΠΈΡ ΝΩΘΡΟΎ ΚΥΝΌΣ
+//$str = mb_convert_case($str, MB_CASE_TITLE, "UTF-8");
+//echo $str; // Prints Τάχιστη Αλώπηξ Βαφήσ Ψημένη Γη, Δρασκελίζει Υπέρ Νωθρού Κυνόσ
+    
+    
+    function testmb_ucwords() {
+        $this->assertEquals('Daniel Ackroyd', mb_ucwords('daniel ackroyd'));
+        $this->assertEquals('Daniel Ackroyd', mb_ucwords('Daniel Ackroyd'));
+    }
+
+    function testmb_str_split() {
+        
+        $string = "DanielAckers";
+        
+        $result = mb_str_split($string, 6);
+        
+        $this->assertEquals(2, count($result));
+        $this->assertEquals($result[0], 'Daniel');
+        $this->assertEquals($result[1], 'Ackers');
+
+        $string = "DanielAckers";
+
+        $result = mb_str_split($string);
+        $this->assertEquals(mb_strlen($string), count($result));
+    }
+
+    function testmb_strcasecmp() {
+        $result =  mb_strcasecmp('Daniel', 'DANIEL');
+        $this->assertEquals(0, $result);
+    }
+
+    function testmb_strrev() {
+        $result = mb_strrev('Daniel');
+
+        $this->assertEquals('leinaD', $result);
+    }
+
+    function testmb_substr_replace() {
+    //    mb_substr_replace($string, $replacement, $start, $length = null, $encoding = null)
+        $var = 'ABCDEFGH:/MNRPQR/';
+        $this->assertEquals('ABCDEFGH:/bob/', mb_substr_replace($var, 'bob', 10, -1));
+
+        $this->assertEquals('ABCDEFGH:/MNRPQR/bob', mb_substr_replace($var, 'bob', 20, 0));
+
+        $this->assertEquals('ABCDEFGH:/bob', mb_substr_replace($var, 'bob', 10, 10));
+
+        $this->assertEquals('ABCDEFGH:/bob/', mb_substr_replace($var, 'bob', -7, -1));
+
+        $this->assertEquals('bob', mb_substr_replace($var, 'bob', 0));
+        $this->assertEquals('bob', mb_substr_replace($var, 'bob', 0, strlen($var)));
+        $this->assertEquals('bob', mb_substr_replace($var, 'bob', 0, strlen($var) + 5));
+
+        $this->assertEquals('bob', mb_substr_replace($var, 'bob', 0, null, "UTF-8"));
+        
+    }
+
+    function testmb_wordwrap() {
+ 
+        
+        $string = 'thislisalongstring';
+        $width = strlen($string);
+        $expectedResult = $string."\n".$string."\n".$string;
+
+        $string = $string.$string.$string;
+        $result = mb_wordwrap($string, $width, "\n", true);
+        $this->assertEquals($expectedResult, $result);
+
+        $text = "A very long woooooooooord.";
+        $result = mb_wordwrap($text, 8, "|", true);
+        $this->assertEquals("A very|long|wooooooo|ooord.", $result);
+
+        $expected = "The quick brown fox<br />\njumped over the lazy<br />\ndog.";
+        $text = "The quick brown fox jumped over the lazy dog.";
+        $result = mb_wordwrap($text, 20, "<br />\n");
+        $this->assertEquals($expected, $result);
+    }
+
+    function testmb_str_replace() {
+        $subject = "longreplacephrase";
+        $search  = 'replace';
+        $replace = 'changed';
+        
+        $result = mb_str_replace($search, $replace, $subject);
+        $this->assertEquals('longchangedphrase', $result);
+
+        $subject  = ["You should eat fruits, vegetables, and fiber every day."];
+        $search = array("fruits", "vegetables", "fiber");
+        $replace   = array("pizza", "beer", "ice cream");
+
+        $result = mb_str_replace($search, $replace, $subject);
+        $this->assertEquals("You should eat pizza, beer, and ice cream every day.", $result[0]);
+    }
+}
+
